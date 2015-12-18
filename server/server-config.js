99eb9408ed717b195ff653d1f39c60951b5cbf33
seanagibson <seanandgibson@gmail.com>
2015-12-18 15:53:56 -0800

    [feat]: Adds recipes route and mock data

--- server/server-config.js
+++ server/server-config.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var partials = require('express-partials');
 var morgan = require('morgan');
-
+var db = require('./data.js');
 
 var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/../client'));
 
+app.get('/recipes', function(req, res){
+  res.status(200);
+  res.json(db);
+});
 
 module.exports = app;
