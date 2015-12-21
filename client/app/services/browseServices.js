761c4c2a530b9ab0a82a3fccb5f728619f6d9ffb
seanagibson <seanandgibson@gmail.com>
2015-12-21 13:42:01 -0800

    [feat]: Adds a get request for all recipes

--- client/app/services/browseServices.js
+++ client/app/services/browseServices.js
@@ -1,4 +1,15 @@
 angular.module('tinnr.browseServices', [])
   .factory('Browse', function($http) {
 
+    //Gets all recipes from the server
+    var getAll = function () {
+      return $http({
+        method: 'GET',
+        url: '/recipes'
+      })
+      .then(function (resp) {
+        return resp.data;
+      });
+    };
+    
   });
