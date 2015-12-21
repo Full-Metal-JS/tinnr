fa079a6db986012002f1b8a3af576110753de674
seanagibson <seanandgibson@gmail.com>
2015-12-21 13:08:14 -0800

    [feat]: Add route to get all recipes

--- server/server-config.js
+++ server/server-config.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var partials = require('express-partials');
 var morgan = require('morgan');
-
+var path = require('path');
+var db = require('./data.js');
 
 var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(path.join(__dirname, '/../client')));
 
+//Returns a object with property results containing array of recipes
+app.get('/recipes', function(req, res) {
+  res.status(200);
+  res.json(db);
+});
 
 module.exports = app;
