11969e1f0f47911eb5597d417f6845a16d787867
Ian Bari <ian.bari@att.net>
2015-12-23 14:54:54 -0800

    [feat]: add user auth views

---
+++ client/app/services/authServices.js
@@ -0,0 +1,37 @@
+angular.module('tinnr.authServices', [])
+  .factory('Auth', function ($http, $state, $window) {
+    var auth = {};
+
+    auth.signin = function (user) {
+      return $http({
+        method: 'POST',
+        url: '/api/users/signin',
+        data: user
+      })
+      .then(function (resp) {
+        return resp.data.token;
+      });
+    };
+
+    auth.signup = function (user) {
+      return $http({
+        method: 'POST',
+        url: '/api/users/signup',
+        data: user
+      })
+      .then(function (resp) {
+        return resp.data.token;
+      });
+    };
+
+    auth.isAuth = function () {
+      return !!$window.localStorage.getItem('com.tinnr');
+    };
+
+    auth.signout = function () {
+      $window.localStorage.removeItem('com.tinnr');
+      $state.go('/signin');
+    };
+
+    return auth;
+  });
