2d05e82bed2c6c486bff5f51ef7b97e1d9cc367c
Elliot S Chi <elliotschi@gmail.com>
2015-12-23 15:09:25 -0800

    Revert "[feat]: add user auth views"

--- server/user/userRoutes.js
+++ server/user/userRoutes.js
@@ -1,7 +1,7 @@
 var userController = require('./userController.js');
 
 module.exports = function(app) {
-  app.post('/signin', userController.signin);
+  app.post('/login', userController.signin);
   app.post('/signup', userController.signup);
   app.post('/signedin', userController.checkAuth);
   app.get('/meals', userController.getSavedMeals);
