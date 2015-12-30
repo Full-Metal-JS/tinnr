10e1b033a289771bce6da63ac5638991d7cbfae2
seanagibson <seanandgibson@gmail.com>
2015-12-29 16:19:06 -0800

    [test]: Adds tests for server - user routes

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -2,20 +2,95 @@ var expect = require('chai').expect;
 var request = require('request');
 
 var db = require('../server/data.js');
+var User = require('../server/user/userModel.js');
 
-
-describe('Server routes:', function() {
+describe('', function() {
   var req = request.defaults();
 
-  it('/recipes: Responds with all recipes', function() {
+  beforeEach(function() {
+
+  });
+
+  describe('Server route: /api/recipes', function() {
+
+    it('Responds with all recipes', function() {
+      var options = {
+        'method': 'GET',
+        'uri': 'http://127.0.0.1:3000/api/recipes'
+      };
+
+      req(options, function(error, res, body) {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.length).to.equal(db.matches.length);
+        done();
+      });
+    });
+  });
+
+  describe('Server route: /api/user/signup', function() {
     var options = {
-      'method': 'GET',
-      'uri': 'http://127.0.0.1:3000/recipes'
+      'method': 'POST',
+      'uri': 'http://127.0.0.1:3000/api/user/signup',
+      'json': {
+        'username': 'test@test.com',
+        'password': 'password'
+      }
     };
 
-    req(options, function(error, res, body) {
-      expect(res.statusCode).to.equal(200);
-      expect(res.body.results.length).to.equal(db.rresults.length);
+    it('Should create a new user', function() {
+      var foundUser;
+      req(options, function(error, res, body) {
+        expect(res.statusCode).to.equal(200);
+        expect(body.token).to.not.be.null;
+        done();
+      });
+      var userQuery = User.where({username: 'test@test.com'});
+      userQuery.findOne(function(err, dbUser) {
+        foundUser = dbUser;
+      });
+      expect(foundUser).to.not.be.null;
+    });
+
+    it('Should not create a new user with same username', function() {
+      req(options, function(error, res, body) {
+        expect(res.statusCode).to.equal(403);
+        done();
+      });
+    });
+  });
+
+  describe('Server route: /api/user/signin', function() {
+    it('Should signin an existing user', function() {
+      var options = {
+        'method': 'POST',
+        'uri': 'http://127.0.0.1:3000/api/user/signin',
+        'json': {
+          'username': 'test@test.com',
+          'password': 'password'
+        }
+      };
+
+      req(options, function(error, res, body) {
+        expect(res.statusCode).to.equal(200);
+        expect(body.token).to.not.be.null;
+        done();
+      });
+    });
+
+    it('Should not signin a user with incorrect password', function() {
+      var options = {
+        'method': 'POST',
+        'uri': 'http://127.0.0.1:3000/api/user/signin',
+        'json': {
+          'username': 'test@test.com',
+          'password': 'pass'
+        }
+      };
+
+      req(options, function(error, res, body) {
+        expect(res.statusCode).to.equal(401);
+        done();
+      });
     });
   });
 });
